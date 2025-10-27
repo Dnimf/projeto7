2c558964fdbd610c95fdc52833fad36111aca154
Rafael Vieira Bravo <rafaelvb@al.insper.edu.br>
2025-10-27 12:26:45 -0300

    listas de bandas e git reset

--- filtro7.py
+++ filtro7.py
@@ -1,2 +1,18 @@
+<<<<<<< Updated upstream
 # oi
 from  filtros import *
+=======
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.signal import butter, iirnotch
+
+bandas = [
+    20, 32, 64,
+    125, 250, 500,
+    1e3, 2e3, 4e3,
+    8e3, 16e3, 20e3
+]
+
+magnitudes = np.arange(start=-10, stop=12, step=2)
+print(magnitudes)
+>>>>>>> Stashed changes
