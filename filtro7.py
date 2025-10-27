667173f18e57a5eb6890e781addc2e13ef3bc33d
Dnimf <danielmf1@al.insper.edu.br>
2025-10-27 12:39:58 -0300

    captacao de audio

--- filtro7.py
+++ filtro7.py
@@ -2,6 +2,7 @@ from  filtros import *
 import matplotlib.pyplot as plt
 import numpy as np
 from scipy.signal import butter, iirnotch
+import sounddevice as sd
 
 bandas = [
     20, 32, 64,
@@ -9,6 +10,18 @@ bandas = [
     1e3, 2e3, 4e3,
     8e3, 16e3, 20e3
 ]
-
+# gravacao
+T = 2
+fs=44100
+gain_db=0
+Q=1
+numAmostras = fs*T
+audio = sd.rec(int(numAmostras),fs, channels=1)
+sd.wait()
+# cria equacao
 magnitudes = np.arange(start=-10, stop=12, step=2)
 print(magnitudes)
+gain_db=float(input("ganho em db"))
+f0=0
+Q=1
+b, a = peaking_eq(f0,gain_db,Q,fs)
